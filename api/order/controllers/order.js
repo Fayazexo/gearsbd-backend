e77f2177d582cebeac55fe95c9ef9c95bfc38577
Fayaze H. Pantho <fayazexo@gmail.com>
2021-12-24 16:22:33 +0600

    Changed permissions

---
+++ api/order/controllers/order.js
@@ -0,0 +1,34 @@
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
+
+const fromDecimalToInt = (number) => parseInt(number * 100);
+
+module.exports = {
+  async find(ctx) {
+    const { user } = ctx.state;
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.order.search({
+        ...ctx.query,
+        user: user.id,
+      });
+    } else {
+      entities = await strapi.services.order.find({
+        ...ctx.query,
+        user: user.id,
+      });
+    }
+
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.order })
+    );
+  },
+
+  async findOne(ctx) {
+    const { id } = ctx.params;
+    const { user } = ctx.state;
+
+    const entity = await strapi.services.order.findOne({ id, user: user.id });
+    return sanitizeEntity(entity, { model: strapi.models.order });
+  },
+};
